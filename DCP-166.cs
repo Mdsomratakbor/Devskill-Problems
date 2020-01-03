a1f8303619db0c45e3ca18bffa1e637eb031959d
Mdsomratakbor <53125546+Mdsomratakbor@users.noreply.github.com>
2020-01-03 22:11:49 +0600

    DCP-166

---
+++ DCP-166.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Point_Of_Sales
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Totalinput = Int32.Parse(Console.ReadLine());
+            int Case = 0;
+            List<string> mylist = new List<string>();
+            for (int i = 0; i < Totalinput; i++)
+            {
+                int TotalProductBuy = Int32.Parse(Console.ReadLine());
+
+                var ProductPrice = 0.00;
+                double TotalPrice;
+                for (int j = 0; j < TotalProductBuy; j++)
+                {
+                    var line = Console.ReadLine();
+                    var data = line.Split(' ');
+                    double Price = double.Parse(data[0]);
+                    float Quantity = float.Parse(data[1]);
+                    ProductPrice += Price * Quantity;
+                }
+                int Payableamount = Int32.Parse(Console.ReadLine());
+                TotalPrice = Convert.ToDouble(Payableamount - ProductPrice);
+                TotalPrice = Math.Floor(TotalPrice);
+                Case++;
+                mylist.Add("Case " + Case + ": " + TotalPrice);
+            }
+            foreach (var value in mylist)
+            {
+                Console.WriteLine(value);
+            }
+            Console.Read();
+        }
+    }
+}
