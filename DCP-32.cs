a4e258c058179f3367f5bde45cf58012d027c0c1
Mdsomratakbor <53125546+Mdsomratakbor@users.noreply.github.com>
2020-01-03 21:21:22 +0600

    DCP-32

---
+++ DCP-32.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_Grid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int userInput = Int32.Parse(Console.ReadLine());
+            List<string> mylist = new List<string>();
+            int Case = 0;
+            for (int i=0; i<userInput; i++)
+            {
+                Case++;
+                var data = Console.ReadLine();
+                var line = data.Split(' ');
+                int x1 = Int32.Parse(line[0]);
+                int y1 = Int32.Parse(line[1]);
+                int x2 = Int32.Parse(line[2]);
+                int y2 = Int32.Parse(line[3]);
+                int xy = Math.Abs(x1-x2)+Math.Abs(y1-y2);
+                mylist.Add("Case "+Case+": "+xy);
+            }
+            foreach (var value in mylist)
+            {
+                Console.WriteLine(value);
+            }
+            Console.Read();
+        }
+    }
+}
