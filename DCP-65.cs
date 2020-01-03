67ebaf9b350b12471b81fd00d7f4a54995681381
Mdsomratakbor <53125546+Mdsomratakbor@users.noreply.github.com>
2020-01-03 22:04:22 +0600

    DCP-65

---
+++ DCP-65.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Magical_Bank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int userInput = Int32.Parse(Console.ReadLine());
+            List<int> myconinList = new List<int>();
+            for (int i=0; i<userInput; i++)
+            {
+                int numberCoin = Int32.Parse(Console.ReadLine());
+                double number = Math.Ceiling(((Math.Sqrt(1.0 + (8 * numberCoin)))-1.0)/2.0);
+                int total = Convert.ToInt32(number);
+                myconinList.Add(total);
+            }
+            foreach (var value in myconinList)
+            {
+                Console.WriteLine(value);
+            }
+            Console.Read();
+        }
+    }
+}
