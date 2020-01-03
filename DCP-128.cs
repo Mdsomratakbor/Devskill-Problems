cd3ada3a92fcaf5dbb1b926d2ae09ef4771c2bc2
Mdsomratakbor <53125546+Mdsomratakbor@users.noreply.github.com>
2020-01-03 22:09:40 +0600

    DCP-128

---
+++ DCP-128.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Galib_and_Hasan_Again
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int testInput = Int32.Parse(Console.ReadLine());
+            int i = 1;
+            
+            List<string> mylist = new List<string>();
+            while (i <= testInput)
+            {
+                
+                var data = Console.ReadLine().Split(' ');
+                long AO = long.Parse(data[0]);
+                long BC = long.Parse(data[1]);
+                long AB = AO*2;
+                long AC = (AB*AB) - (BC*BC);
+                mylist.Add("Case "+i+": "+AC);
+                
+                
+                i++;
+            }
+            foreach (var value in mylist)
+            {
+                Console.WriteLine(value);
+            }
+            Console.Read();
+        }
+    }
+}
