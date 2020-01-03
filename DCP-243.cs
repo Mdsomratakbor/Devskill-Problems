c8d4bdbf6ae3e47e5acd5a83ddbd3bd60f7eef55
Mdsomratakbor <53125546+Mdsomratakbor@users.noreply.github.com>
2020-01-03 22:19:37 +0600

    DCP-243

---
+++ DCP-243.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_Power_Of_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int userInput = Int32.Parse(Console.ReadLine());
+            List<int> mylist = new List<int>();
+            for (int i=0; i<userInput; i++)
+            {
+                long number = long.Parse(Console.ReadLine());
+                if (number%2==0)
+                {
+                    mylist.Add(1);
+                }
+                else
+                {
+                    mylist.Add(9);
+                 }
+            }
+            foreach (var value in mylist)
+            {
+                Console.WriteLine(value);
+            }
+            Console.Read();
+        }
+    }
+}
