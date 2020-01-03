76dc63da60fbfeec3c26ebc747810a0b65b4d725
Mdsomratakbor <53125546+Mdsomratakbor@users.noreply.github.com>
2020-01-03 22:07:33 +0600

    DCP-85

---
+++ DCP-85.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tomrrow_Is_Eid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int EidDay1 = 30;
+            int EidDay2 = 29;
+            int UserInput = Int32.Parse(Console.ReadLine());
+            if (EidDay1 == UserInput || EidDay2 == UserInput)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+            Console.ReadLine();
+        }
+    }
+}
