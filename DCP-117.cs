63c15e7205499895d05a42521f7f79f8144ec1f9
Mdsomratakbor <53125546+Mdsomratakbor@users.noreply.github.com>
2020-01-03 22:08:56 +0600

    DCP-117

---
+++ DCP-117.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Not_Easy_But_Not_Hard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int UserInput = Int32.Parse(Console.ReadLine());
+            
+            List<long> mylist = new List<long>();
+            for (int i=0; i<UserInput; i++)
+            {
+
+                long Number = long.Parse(Console.ReadLine());
+                long sum = (Number * (Number + 1)) / 2;
+
+
+                mylist.Add(sum);
+
+            }
+            foreach (var value in mylist)
+            {
+                Console.WriteLine(value);
+            }
+            Console.ReadKey();
+        }
+    }
+}
