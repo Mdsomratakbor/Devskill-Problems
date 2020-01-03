04891a2cf496d942fe4e805705cc3b7451a1f711
Mdsomratakbor <53125546+Mdsomratakbor@users.noreply.github.com>
2020-01-03 22:02:31 +0600

    DCP-46

---
+++ DCP-46.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JU_Programming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int userInput = Int32.Parse(Console.ReadLine());
+            List<decimal> mylist = new List<decimal>();
+            for (int i=0; i<userInput; i++)
+            {
+                var data = Console.ReadLine();
+                var line = data.Split(' ');
+                decimal firstNumber = Convert.ToDecimal(line[0]);
+                decimal seceondNumber = Convert.ToDecimal(line[1]);
+                decimal number = (firstNumber + seceondNumber);
+                number = number / 2;
+                decimal total = Convert.ToDecimal(Math.Floor(number)); 
+                mylist.Add(total);
+                
+            }
+            foreach (var value in mylist)
+            {
+                Console.WriteLine(value);
+            }
+            Console.Read();
+        }
+    }
+}
