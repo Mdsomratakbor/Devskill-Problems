18df68729a1ab1ce173502c93ce05a17bb4eb946
Mdsomratakbor <53125546+Mdsomratakbor@users.noreply.github.com>
2020-01-03 22:25:14 +0600

    DCP-320

---
+++ DCP320.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Print_a_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1");
+            Console.Read();
+        }
+    }
+}
