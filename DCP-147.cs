40dccb06728ef3e295344f2ffb1558407e6928d4
Mdsomratakbor <53125546+Mdsomratakbor@users.noreply.github.com>
2020-01-03 22:10:19 +0600

    DCP-147

---
+++ DCP-147.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Brothers_Challange
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long numberInput = long.Parse(Console.ReadLine());
+            long sum = 0;
+            List<long> mylist = new List<long>();
+            for (int i = 3; i < numberInput; i++)
+                if (i % 3 == 0 || i % 5 == 0)
+                    sum += i;
+            mylist.Add(sum);
+
+            foreach (var value in mylist)
+                Console.WriteLine(value);
+            Console.Read();
+        }
+    }
+}
