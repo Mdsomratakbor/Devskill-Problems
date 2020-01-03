39a3f0d8941ee1f39b69647ce0a37da4a2cb87fc
Mdsomratakbor <53125546+Mdsomratakbor@users.noreply.github.com>
2020-01-03 21:23:50 +0600

    DCP-35

---
+++ DCP-35.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SquareNumbercheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int UserInput = int.Parse(Console.ReadLine());
+            List<string> mylist = new List<string>();          
+            for (int i=1; i<=UserInput; i++)
+            {               
+                int number = int.Parse(Console.ReadLine());
+                double root = Math.Sqrt(number);
+                 root = Math.Round(root);
+                int Total = (int)(root * root);
+                if (number == Total)
+                    mylist.Add("Case " + i + ": YES");
+                else
+                    mylist.Add("Case " + i + ": NO");                                               
+            }
+            foreach (var value in mylist)
+            {
+                Console.WriteLine(value);
+            }
+            Console.ReadLine();
+        }
+    }
+}
