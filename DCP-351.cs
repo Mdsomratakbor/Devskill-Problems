4c0007a7900e6748e994bc837947caaeeb1263a6
Mdsomratakbor <53125546+Mdsomratakbor@users.noreply.github.com>
2020-01-03 22:28:14 +0600

    DCP-351

---
+++ DCP-351.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Epic_Battle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("402");
+            Console.Read();
+        }
+    }
+}
