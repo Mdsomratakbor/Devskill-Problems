ae11e5c2f3b3e4fe5500ac48a1bc8bfda6aca68a
Mdsomratakbor <53125546+Mdsomratakbor@users.noreply.github.com>
2020-01-03 21:14:03 +0600

    DCP-27
    Is It Prime?

---
+++ DCP-27.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prime_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int UserInput = Int32.Parse(Console.ReadLine());
+            List<string> mylist = new List<string>();
+            string Output = " ";
+            for (int i=0; i<UserInput; i++)
+            {
+                int InputNUmber = Int32.Parse(Console.ReadLine());
+                int flag = 0;
+                int DivideNumber = InputNUmber / 2;
+                if (InputNUmber == 1 || InputNUmber == 0)
+                {
+                    Output = "No";
+                }
+                else {
+                    for (int k = 2; k <= DivideNumber; k++)
+                    {
+                        flag = 0;
+                        if (InputNUmber % k == 0)
+                        {
+                            Output = "No";
+                            flag = 1;
+                            break;
+                        }
+
+                    }
+                    if (flag == 0)
+                    {
+                        Output = "Yes";
+                    }
+                    
+                }
+                mylist.Add(Output);
+            }
+            
+            foreach (var value in mylist)
+            {
+                Console.WriteLine(value);
+            }
+            Console.ReadLine();
+        }
+    }
+}
