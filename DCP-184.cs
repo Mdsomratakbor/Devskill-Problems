cba00e063745df7d8ed9309965f4f65f8892fc21
Mdsomratakbor <53125546+Mdsomratakbor@users.noreply.github.com>
2020-01-03 22:14:35 +0600

    DCP-184

---
+++ DCP-184.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Easy_Sequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int UserInput = Int32.Parse(Console.ReadLine());
+            List<int> mylist = new List<int>();
+            int[] myarray = new int[45];
+            myarray[0] = 0;
+            myarray[1] = 1;
+            myarray[2] = 3;
+            for (int j = 3; j < 45; j++)
+            {
+                myarray[j] = myarray[j - 1] + myarray[j - 2];
+            }
+            for (int i=0; i<UserInput; i++)
+            {
+             
+                int Number = Int32.Parse(Console.ReadLine());
+                mylist.Add(myarray[Number]);
+               
+            }
+            foreach (var value in mylist)
+            {
+                Console.WriteLine(value);
+            }
+            Console.Read();
+        }
+
+        
+    }
+}
