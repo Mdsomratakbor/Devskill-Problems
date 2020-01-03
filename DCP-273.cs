7329a5f73bf898534904db34798e2ff26a9d33d0
Mdsomratakbor <53125546+Mdsomratakbor@users.noreply.github.com>
2020-01-03 22:20:59 +0600

    DCP-273

---
+++ DCP-273.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Veley_Easy_Problem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int userInput = Int32.Parse(Console.ReadLine());
+            List<string> mylist = new List<string>();
+            for (int i=0; i<userInput;i++)
+            {
+                string userString = Console.ReadLine();
+                string outputString = "";
+                if (userString.ToLower().Contains('r') == true)
+                {
+                    outputString=  "Not vely easy";
+
+                }
+                else
+                {
+                    outputString = "Vely easy";
+                }
+                mylist.Add(outputString);
+            }
+            foreach (var data in mylist)
+            {
+                Console.WriteLine(data);
+            }
+            Console.Read();
+        }
+    }
+}
