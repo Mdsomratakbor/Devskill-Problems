51c8a5fdeaaa1721bcda55701d4cc62423429eff
Mdsomratakbor <53125546+Mdsomratakbor@users.noreply.github.com>
2020-01-03 22:06:54 +0600

    DCP-82

---
+++ DCP-82.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chicken_Lover
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int UserInput = Int32.Parse(Console.ReadLine());
+            List<string> mylist = new List<string>();
+            string Output = "";
+            for (int i=0; i<UserInput; i++)
+            {
+                int Input = Int32.Parse(Console.ReadLine());
+                int ChickenLover = 120;
+                if (ChickenLover<=Input)
+                {
+                    Output = "Good Boy Sifat";
+                }
+                else
+                {
+                    Output = "Naughty Boy Sifat";
+                }
+                mylist.Add(Output);
+            }
+            foreach (var value in mylist)
+            {
+                Console.WriteLine(value);
+            }
+            Console.ReadLine();
+        }
+    }
+}
