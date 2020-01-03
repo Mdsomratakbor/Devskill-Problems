a9cbb45b130195173d5081f7d450f3fb9a3d4dc5
Mdsomratakbor <53125546+Mdsomratakbor@users.noreply.github.com>
2020-01-03 22:26:00 +0600

    DCP-321

---
+++ DCP-321.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Change_Case_of_Alphabet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int userInput = Int32.Parse(Console.ReadLine());
+            List<StringBuilder> mylist = new List<StringBuilder>();
+            
+            for (int i=0; i<userInput; i++ )
+            {
+                StringBuilder sb = new StringBuilder();
+                string stringDataInput = Console.ReadLine();
+               
+                foreach (char value in stringDataInput)
+                {
+                    if (char.IsUpper(value))
+                    {
+                        sb.Append(char.ToLower(value));
+
+
+                    }
+                    else
+                    {
+                        sb.Append(char.ToUpper(value));
+
+
+                    }
+                }
+                    
+                    mylist.Add(sb);
+                
+
+            }
+            foreach (var value in mylist)
+            {
+                Console.WriteLine(value);
+            }
+            Console.ReadKey();
+        }
+    }
+}
