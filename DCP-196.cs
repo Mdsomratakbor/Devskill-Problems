296decbc22577e4c75b5bfd4ea2e25b2c922d54a
Mdsomratakbor <53125546+Mdsomratakbor@users.noreply.github.com>
2020-01-03 22:16:43 +0600

    DCP-196

---
+++ DCP-196.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Break_Simulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = Int32.Parse(Console.ReadLine());
+
+            List<decimal> list = new List<decimal>();
+            for (int i=0; i<number; i++)
+            {
+                var line = Console.ReadLine();
+                var data = line.Split(' ');//data input two value in space
+                decimal V = decimal.Parse(data[0]); //first integer
+                decimal T = decimal.Parse(data[1]);//seceond integer
+                decimal S =-(V / T);//nagative output print
+                S = Math.Round(S, 2);//two decimal number output
+                list.Add(S);
+                
+            }
+            foreach (var result in list)
+            {
+                Console.WriteLine(result);
+            }
+
+            Console.Read();
+
+        }
+    }
+}
