4f59c3ea1f260fc0860e1ca221514cd4331ed943
Mdsomratakbor <53125546+Mdsomratakbor@users.noreply.github.com>
2020-01-03 21:09:54 +0600

    DCP-25
    Palindrome

---
+++ DCP-25.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palingdrom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int range = int.Parse(Console.ReadLine());
+            List<string> list = new List<string>();
+
+            for(int k = 0; k < range; k++)
+            {
+                
+                string word = Console.ReadLine();
+
+                string reverseword = "";
+                char[] firstword = word.ToLower().ToCharArray();
+                for (int i = word.Length - 1; i >= 0; --i)
+                {
+                    reverseword += word[i];
+
+                }
+                char[] seceondword = reverseword.ToLower().ToCharArray();
+
+                
+                
+                    if (firstword[0] == seceondword[0])
+                    {
+                        list.Add("Yes");
+                        //Console.WriteLine("Yes");
+
+                    }
+                    else if (firstword[0]== seceondword[0] && firstword[1] == seceondword[1])
+                {
+                    list.Add("Yes");
+                }
+                    else
+                    {
+                        list.Add("No");
+                        //Console.WriteLine("No");
+                    }
+                }
+            
+
+            foreach( var value in list)
+            {
+                Console.WriteLine(value);
+            }
+           
+            Console.Read();
+
+
+        }
+    }
+}
