68f23820a90de4823388b9d08b15031e6395dc77
Mdsomratakbor <53125546+Mdsomratakbor@users.noreply.github.com>
2020-01-03 22:23:50 +0600

    DCP-309

---
+++ DCP-309.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interest_Research
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("7059");
+            Console.Read();
+        }
+    }
+}
