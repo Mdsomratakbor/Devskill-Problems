66b340efacc6dbedd5e21a5d8e8275d62adf3656
Mdsomratakbor <53125546+Mdsomratakbor@users.noreply.github.com>
2020-01-03 21:11:58 +0600

    DCP-26
    Reverse

---
+++ DCP-26.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = Int32.Parse(Console.ReadLine());
+            List<string> mylist = new List<string>();
+            for (int k=0; k<input; k++) {
+                string Userinput = Console.ReadLine();
+                string OutputWord = "";
+                for (int i = Userinput.Length - 1; i >= 0; --i)
+                {
+                    OutputWord += Userinput[i];
+                }
+                mylist.Add(OutputWord);
+            }
+            foreach (var value in mylist)
+            {
+                Console.WriteLine(value);
+            }
+
+            Console.ReadLine();
+        }
+
+    }
+}
