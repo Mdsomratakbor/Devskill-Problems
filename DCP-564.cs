f3b1ce74f151433db5d7eae6921b0383fb872e85
Mdsomratakbor <53125546+Mdsomratakbor@users.noreply.github.com>
2020-01-06 19:15:18 +0600

    DCP-564

---
+++ DCP-564.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Divide_and_Rule
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int testInput = int.Parse(Console.ReadLine());
+            List<string> mylist = new List<string>();
+            StringBuilder zeroDigit = new StringBuilder();
+             for (int i=1; i<=testInput; i++)
+            {
+                int digitNumber = int.Parse(Console.ReadLine());
+                if (digitNumber == 1)
+                {
+                    mylist.Add(3.ToString());
+                }
+                else
+                {
+                    for (int j = 2; j <= (digitNumber - 1); j++)
+                    {
+                        zeroDigit.Append(0);
+                    }
+                    mylist.Add(1+""+zeroDigit+2);
+                }
+                zeroDigit = new StringBuilder();
+                
+            }
+            foreach (var value in mylist)
+            {
+                Console.WriteLine(value);
+            }
+
+            Console.Read();
+        }
+    }
+}
