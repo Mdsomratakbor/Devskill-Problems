a5db0fd98eece96e497859f66aeada48c3ba23ff
Mdsomratakbor <53125546+Mdsomratakbor@users.noreply.github.com>
2020-01-06 19:03:15 +0600

    DCP-458

---
+++ DCP-458.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_Sophia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine(@"12\07\2017");
+            Console.Read();
+        }
+    }
+}
