aa746a796d6d874228fbb9531713a030f5a830a5
Mdsomratakbor <53125546+Mdsomratakbor@users.noreply.github.com>
2020-01-06 19:12:38 +0600

    DCP-539

---
+++ DCP-539.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kufa_Land
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int userInput = Int32.Parse(Console.ReadLine());
+            List<int> mylist = new List<int>();
+            
+            for (int i=0; i<userInput; i++)
+            {
+                long number = long.Parse(Console.ReadLine());
+                if (number % 3 == 0)
+                    mylist.Add(1);
+                else if (number % 3 == 1)
+                    mylist.Add(2);
+                else
+                    mylist.Add(4);
+
+            }
+            foreach (var value in mylist)
+                Console.WriteLine(value);
+            Console.Read();
+        }
+    }
+}
