466baafcd85fbfba3a505f58cc5bc6fdbef180c6
Mdsomratakbor <53125546+Mdsomratakbor@users.noreply.github.com>
2020-01-06 18:59:33 +0600

    DCP-420

---
+++ DCP-420.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minimum_Cost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int testCase = int.Parse(Console.ReadLine());
+            List<int> mylist = new List<int>();
+           
+            for (int i=0; i<testCase; i++)
+            {
+                int numberElement = int.Parse(Console.ReadLine());
+                var myArray = new int[numberElement];
+                var data = Console.ReadLine().Split(' ');
+                for (int j=0; j<numberElement; j++)
+                {
+                    myArray[j] = int.Parse(data[j]);
+                }
+
+                int minimum = 0;              
+                Array.Sort(myArray);
+                minimum = myArray[0];
+                
+
+                int sum = 0;
+                for (int k = 1; k<myArray.Length; k++)
+                {
+
+                    sum +=  myArray[k] - minimum;                          
+                          
+                }
+                mylist.Add(sum);
+            }
+            foreach (var value in mylist)
+                Console.WriteLine(value);
+            Console.Read(); 
+        }
+    }
+}
