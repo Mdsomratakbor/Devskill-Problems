a89aec347afa440cde3139ce75cd706c19baf09e
Mdsomratakbor <53125546+Mdsomratakbor@users.noreply.github.com>
2020-01-06 19:09:31 +0600

    DCP-517

---
+++ DCP-517.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FIFA_2030
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int testCase = int.Parse(Console.ReadLine());
+            List<string> mylist = new List<string>();
+            for (int i=0; i<testCase; i++)
+            {
+                int testCountry = int.Parse(Console.ReadLine());
+                string answer = "";
+                int maximum = -1;
+                for (int j=0; j<testCountry; j++)
+                {
+                    var data = Console.ReadLine().Split(' ');
+                    string country = data[0].ToString();
+                    int totalGoal = int.Parse(data[1]);                   
+                    
+                    if (totalGoal>maximum)
+                    {
+                        maximum = totalGoal;
+                        answer = country;
+                    }
+                }
+                mylist.Add(@"Winner: \"+answer+"/");
+
+            }
+            foreach (var value in mylist)
+            {
+                Console.WriteLine(value);
+            }
+            Console.Read();
+
+        }
+    }
+}
