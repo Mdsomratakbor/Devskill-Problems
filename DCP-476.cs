5d469260d3bcb99b103f3a86177aab5d50d42307
Mdsomratakbor <53125546+Mdsomratakbor@users.noreply.github.com>
2020-01-06 19:05:54 +0600

    DCP-476

---
+++ DCP-476.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Submission_Verdict
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int userInput = Int32.Parse(Console.ReadLine());
+            int i = 0;
+            List<string> mylist = new List<string>();
+            while(i<userInput)
+            {
+                string input = Console.ReadLine();
+                if (input.Contains("ac"))
+                    mylist.Add("Accepted");
+                else if (input.Contains("wa"))
+                    mylist.Add("Wrong Answer");
+                else if (input.Contains("rte"))
+                    mylist.Add("Run Time Error");
+                else
+                    mylist.Add("Time Limit Exceeded");
+
+                i++;
+            }
+            foreach (var vlaue in mylist)
+                Console.WriteLine(vlaue);
+            Console.Read();
+                
+        }
+    }
+}
