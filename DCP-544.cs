dba821127871074bd12a05beff0f1fc882dc00a4
Mdsomratakbor <53125546+Mdsomratakbor@users.noreply.github.com>
2020-01-06 19:13:52 +0600

    DCP-544

---
+++ DCP-544.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GAMORA_Confusion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int testCase = int.Parse(Console.ReadLine());
+            List<string> mylist = new List<string>();
+            for (int i=0;i<testCase; i++)
+            {
+                string inputCase = Console.ReadLine();
+                if (inputCase[0] == 'Q')
+                    mylist.Add("I am going to ask you this one time, where is Gamora?");
+                else if (inputCase[0] == 'S')
+                    mylist.Add("I will do you one better, who is Gamora?");
+                else if (inputCase[0] == 'D')
+                    mylist.Add("I will do you one better, why is Gamora?");
+                else
+                    mylist.Add("What is Gamora?");
+            }
+            foreach (var value in mylist)
+            {
+                Console.WriteLine(value);
+            }
+            Console.Read();
+        }
+    }
+}
