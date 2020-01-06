c2c1c4cd4c717bebb96f47d74c9da2e4c00952a7
Mdsomratakbor <53125546+Mdsomratakbor@users.noreply.github.com>
2020-01-06 19:08:48 +0600

    DCP-500

---
+++ DCP-500.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wink_kick
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int userInput = Int32.Parse(Console.ReadLine());
+            List<string> mylist = new List<string>();
+            for (int i = 0; i < userInput; i++)
+            {
+                var data = Console.ReadLine().Split(' ');
+                int A = Convert.ToInt32(data[0]);
+                int B = Convert.ToInt32(data[1]);
+                if (B == 0 || A % B != 0)
+                    mylist.Add(":kick:");
+                else
+                  mylist.Add(":wink:");
+
+            }
+            foreach (var value in mylist)
+            {
+                Console.WriteLine(value);
+            }
+            Console.ReadLine();
+        }
+    }
+}
