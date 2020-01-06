9d36c73a104ff4e8b3d396f0d18d80717683443f
Mdsomratakbor <53125546+Mdsomratakbor@users.noreply.github.com>
2020-01-06 19:02:36 +0600

    DCP-443

---
+++ DCP-443.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boltu_vs_Balti
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int testInput = int.Parse(Console.ReadLine());
+            List<string> mylist = new List<string>();
+            for (int i=1; i<=testInput; i++)
+            {
+                int numberInput = int.Parse(Console.ReadLine());
+                if (numberInput == 1 || numberInput == 3)
+                {
+                    mylist.Add("Case "+i+ "-> YES;");
+                }
+                else
+                {
+                    mylist.Add("Case " + i + "-> NO;");
+                }
+            }
+            foreach (var value in mylist)
+            {
+                Console.WriteLine(value);
+            }
+            Console.Read();
+        }
+    }
+}
