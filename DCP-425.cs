4ed1aab1920c2b9dd648189c577fe76522a22995
Mdsomratakbor <53125546+Mdsomratakbor@users.noreply.github.com>
2020-01-06 19:01:01 +0600

    DCP-425

---
+++ DCP-425.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace What_life_has_taught_you_up_now
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("9");
+            Console.Read();
+        }
+    }
+}
