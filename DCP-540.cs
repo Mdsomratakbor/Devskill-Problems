7737f5b4fc85a3d40bd1ee41cf1b56c52cba2f98
Mdsomratakbor <53125546+Mdsomratakbor@users.noreply.github.com>
2020-01-06 19:13:17 +0600

    DCP-540

---
+++ DCP-540.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pile_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int testCase = int.Parse(Console.ReadLine());
+            List<string> mylist = new List<string>();
+            for (int i=0; i<testCase; i++)
+            {
+                int inputNumber = int.Parse(Console.ReadLine());
+                if (inputNumber % 3 != 0)
+                    mylist.Add("First");
+                else
+                    mylist.Add("Second");
+                
+            }
+            foreach (var value in mylist)
+            {
+                Console.WriteLine(value);
+            }
+            Console.Read();
+        }
+    }
+}
