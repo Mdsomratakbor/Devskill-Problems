fb69229d5d9b44cc4df304af004eed8b24f4bd8d
Mdsomratakbor <53125546+Mdsomratakbor@users.noreply.github.com>
2020-01-06 18:58:47 +0600

    DCP-394

---
+++ DCP-394.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Square_of_N
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {       
+            while (true)
+            {
+                StringBuilder AddNumber1 = new StringBuilder();
+                StringBuilder AddNumber2 = new StringBuilder();
+                var data = Console.ReadLine();
+                if (string.IsNullOrEmpty(data))
+                {
+                    break;
+                }
+               for (int i = 1; i < data.Length; i++)
+                 {
+                    Console.Write(9); 
+                 }
+                Console.Write(8);
+                for (int i = 1; i < data.Length; i++)
+                {
+                    Console.Write(0);
+                }
+                Console.WriteLine(1);
+            }
+            Console.Read();
+
+
+        }
+    }
+}
