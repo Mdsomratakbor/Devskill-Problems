0c23f7dc0660582ab070755ab668e789bab6a004
Mdsomratakbor <53125546+Mdsomratakbor@users.noreply.github.com>
2020-01-06 19:07:26 +0600

    DCP-483

---
+++ DCP-483.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mr.Typical
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "";
+            List<string> mylist = new List<string>();
+
+                str = Console.ReadLine();
+                if (str == "never" || str == "be" || str == "so" || str == "happy" || str == "when" || str == "you" || str == "are" ||
+                 str == "and" || str == "sad")
+                    mylist.Add("YES");
+                else
+                    mylist.Add("NO");
+
+               
+
+            
+            foreach (var value in mylist)
+            {
+                Console.WriteLine(value);
+            }
+            Console.Read();
+        }
+    }
+}
