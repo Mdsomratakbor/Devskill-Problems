c2dbe7c0b1b696f5959f9620471e18e8314093e7
Mdsomratakbor <53125546+Mdsomratakbor@users.noreply.github.com>
2020-05-13 20:54:08 +0600

    The kth Difference
    The kth Difference

---
+++ DCP-558.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_kth_Difference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int userInput = int.Parse(Console.ReadLine());
+            List<int> mylist = new List<int>();
+            while (userInput>0)
+            {
+                Int64 kthGroup = Int64.Parse(Console.ReadLine());
+                kthGroup *= 2;
+                int oddNumber = 0, evenNumber = 0;
+                for(int i=0; i<kthGroup; i++)
+                {
+                    if (i % 2 != 0)
+                        oddNumber += i;
+                    else
+                        evenNumber += i;
+                }
+                int defference = oddNumber - evenNumber;
+                mylist.Add(defference);
+                userInput--;
+            }
+            foreach (var data in mylist)
+            {
+                Console.WriteLine(data);
+            }
+            Console.Read(); 
+        }
+    }
+}
